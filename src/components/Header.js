cf3356a8d8efdce447699e4bd41abeed166f7bab
Choudhury555 <choudhurypanda143@gmail.com>
2023-09-28 03:27:40 +0530

    Header Component Animation,New Resume,Image size reduce

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,46 @@
-import React,{useState} from "react"
+import React,{useState,useEffect} from "react"
 
 function Header(){
+    const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Software Engineer", "Web Developer", "Programmer"];
+  const period = 1000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
 
     const [mobilemenu,setMobilemenu]=useState("close");
 
@@ -28,7 +68,8 @@ function Header(){
             </nav>
         </div>
             <div className="header-text">
-                <p>DEVELOPER/PROGRAMMER</p>
+                <h1>{`⁅→`}<span style={{color:"lightgreen"}}>{text}</span></h1>
+                {/* <p>DEVELOPER/PROGRAMMER</p> */}
                 <h1>Hi,I am Choudhury<span> Abhisek</span><br />From Odisha,India</h1>
             </div>
         </div>
